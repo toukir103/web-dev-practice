dc323302edbb3a3a1f5da6e4fdde660976c739ba
Toukir <toukirahmed.th@gmail.com>
2025-10-12 02:14:00 +0600

    react router setup

---
+++ firebaseAuth/src/components/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1>I am home component</h1>
+        </div>
+    );
+};
+
+export default Home;
--- firebaseAuth/src/main.jsx
+++ firebaseAuth/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import './index.css';
+import Root from './layout/Root';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Root/> ,
+  },
+]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />,
   </StrictMode>,
 )
