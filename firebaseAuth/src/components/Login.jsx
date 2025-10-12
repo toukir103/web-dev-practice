982918701b10c78acef030f27fd4dee57a5d5f18
Toukir <toukirahmed.th@gmail.com>
2025-10-13 00:54:15 +0600

    last update

--- firebaseAuth/src/components/Login.jsx
+++ firebaseAuth/src/components/Login.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../firebase/firebase.init";
 
 const Login = () => {
-    return (
-        <div>
-            <h2>Please login</h2>
-        </div>
-    );
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleSignin = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log("User Info:", result.user);
+      })
+      .catch((error) => {
+        console.error("Login Error:", error.message);
+      });
+  };
+
+  return (
+    <div>
+      <h2>Please login</h2>
+      <button onClick={handleGoogleSignin}>Sign in with Google</button>
+    </div>
+  );
 };
 
 export default Login;
